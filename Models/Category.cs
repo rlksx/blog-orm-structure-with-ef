5e340591e567c006052d362515ef99ecca7b57ab
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-11-26 18:55:36 -0300

    style: some notes in the code

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace blog_orm_structure_with_ef.Models
-{
+{   
+    [Table("Category")]
     public class Category
     {
+        [Key] //=> explicita a propriedade como uma primary key!
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace blog_orm_structure_with_ef.Models
 {
-    [Table("Tag")]
+    [Table("Tag")] //=> buscará Tag no bando e não "Tags" como esta no DbSet.
     public class Tag
     {
         public int Id { get; set; }
